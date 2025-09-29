0ba0b8ccfa0e6945a9bf1d77b6b4c672926d791d
Sagarpatil2003 <sp4765699@gmal.com>
2025-09-29 20:32:42 +0530

    add the notification comp

---
+++ Travel-budget-app/src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Charts from '../components/Charts';
+import Notifications from '../components/Notifications';
+
+function Dashboard() {
+  return (
+    
+    <div>
+      <h1>hello</h1>
+      <Notifications />
+      <Charts />
+    </div>
+  );
+}
+export default Dashboard;
